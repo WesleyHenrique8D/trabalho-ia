b951e57a2f9a64a00668b3da6ec20b4e45505474
Dario <dariogabriel113@gmail.com>
2018-09-12 01:54:09 -0300

    Jogo - Muda exemplo para jogo.

--- src/br/com/buscaemprofundidade/main/Jogo.java
+++ src/br/com/buscaemprofundidade/main/Jogo.java
@@ -4,11 +4,11 @@ import br.com.buscaemprofundidade.busca.BuscaEmProfundidade;
 import br.com.buscaemprofundidade.entity.No;
 
 public class Jogo {
-	private final static String OBJETIVO = "1238X4765";
-	private final static String ESTADO_ATUAL = "1348627X5";
+    private final static String OBJETIVO = "12345678X";
+    private final static String ESTADO_ATUAL = "12345X786";
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         BuscaEmProfundidade bep = new BuscaEmProfundidade(new No(ESTADO_ATUAL), OBJETIVO);
         bep.busca();
-	}
+    }
 }
